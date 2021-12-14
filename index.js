399fa1797dbdd65538232629eb9dda40077e2253
Sid Saythongphet <sid.saythongphet@gmail.com>
2021-12-13 23:22:49 -0500

    commit tree a24cca804a12c0396449115b1434506b814c02ea

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bathe my dog");
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home");
+
+function wrapAdjective(prompt) {
+    return function(adj = "special") {
+        return `You are ${prompt}${adj}${prompt}!`
+    }
+}
+wrapAdjective("*")("a hard worker");
+wrapAdjective("||")("a dedicated programmer")
